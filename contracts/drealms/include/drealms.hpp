db3cd3cfb4b7a4f737a8e96a880126363ffa3709
craigbranscom <craig.branscom@gmail.com>
2019-08-01 16:00:45 -0700

    v0.2.0 additions

--- contracts/drealms/include/drealms.hpp
+++ contracts/drealms/include/drealms.hpp
@@ -2,7 +2,7 @@
 // 
 // @author Craig Branscom
 // @contract drealms
-// @version v0.1.0
+// @version v0.2.0
 // @copyright defined in LICENSE.txt
 
 #pragma once
@@ -14,13 +14,18 @@
 #include <eosio/singleton.hpp>
 #include <eosio/ignore.hpp>
 
+// #include <map>
+
 using namespace std;
 using namespace eosio;
 
-//TODO: add families to realmdata nfts vector in createnft
-//TODO: add symbol to realmdata fts vector in create
-//TODO: rename createnft() to definenft()
-//TODO: rename issuenft() to mintnft()
+//TODO: update fungibles and nonfungibles vectors when creating new schema or fts
+//TODO: design next_level formulae
+//TODO: refactor table opens
+
+//TODO?: rename issuenft() to mintnft()
+//TODO?: move relatives and checksums to separate table
+//TODO?: change license key to license_name
 
 CONTRACT drealms : public contract {
 
@@ -35,54 +40,67 @@ public:
     //sets realmdata singleton
     ACTION setrealmdata(string drealms_version, name realm_name);
 
-    //======================== nonfungible actions ========================
+    //======================== schema/nonfungible actions ========================
 
-    //creates a new token stat, initially sets licensing to disabled
-    ACTION createnft(name new_token_family, name issuer, bool retirable, bool transferable, bool consumable, uint64_t max_supply);
+    //creates a new nft schema, initially sets licensing to disabled
+    ACTION newnftschema(name new_schema_name, name issuer, uint64_t max_supply, symbol exp_symbol,
+        bool retirable, bool transferable, bool consumable, bool activatable);
 
-    //issues a new NFT token
-    ACTION issuenft(name to, name token_family, string memo, bool log);
+    //toggles a schema setting
+    ACTION toggle(name schema_name, name setting_name);
+
+    //adds a stat to defaults
+    ACTION addstat(name schema_name, name stat_name, uint32_t default_value);
+
+    //syncs a token with the map of stats in schema
+    ACTION syncstats(name schema_name, uint64_t serial);
+
+    //issues a new nft
+    ACTION issuenft(name to, name schema_name, string memo, bool log);
 
     //retires nft(s) of a single token name, if retirable
-    ACTION retirenft(name token_family, vector<uint64_t> serials, string memo);
+    ACTION retirenft(name schema_name, vector<uint64_t> serials, string memo);
 
     //transfers nft(s) of a single token name to recipient account, if transferable
-    ACTION transfernft(name from, name to, name token_family, vector<uint64_t> serials, string memo);
+    ACTION transfernft(name from, name to, name schema_name, vector<uint64_t> serials, string memo);
 
     //consumes an nft, if consumable
-    ACTION consumenft(name token_family, uint64_t serial, string memo);
+    ACTION consumenft(name schema_name, uint64_t serial, string memo);
+
+    //activate and nft, if activatable
+    ACTION activatenft(name schema_name, uint64_t serial, string memo);
 
     //updates a checksum if found, inserts if not found
-    ACTION newchecksum(name token_family, name license_owner, uint64_t serial, string new_checksum);
+    ACTION newchecksum(name schema_name, name license_owner, uint64_t serial, string new_checksum);
 
     //logs an nfts data
-    ACTION lognft(name to, name token_family, uint64_t serial);
+    ACTION lognft(name to, name schema_name, uint64_t serial);
 
     //======================== licensing actions ========================
 
     //sets new license model on existing token stats
-    ACTION setlicmodel(name token_family, name new_license_model);
+    ACTION setlicmodel(name schema_name, name new_license_model);
 
     //adds a new license
-    ACTION newlicense(name token_family, name owner, time_point_sec expiration);
+    ACTION newlicense(name schema_name, name owner, time_point_sec expiration);
 
     //erases a license
-    ACTION eraselicense(name token_family, name license_owner);
+    ACTION eraselicense(name schema_name, name license_owner);
 
     //sets minimum and maximum license lengths
-    ACTION setlicminmax(name token_family, uint32_t min_license_length, uint32_t max_license_length);
+    ACTION setlicminmax(name schema_name, uint32_t min_license_length, uint32_t max_license_length);
 
     //sets a license's checksum algorithm
-    ACTION setalgo(name token_family, name license_owner, string new_checksum_algo);
+    ACTION setalgo(name schema_name, name license_owner, string new_checksum_algo);
 
     //updates a license's ATI
-    ACTION setati(name token_family, name license_owner, string new_ati_uri);
+    ACTION setati(name schema_name, name license_owner, string new_ati_uri);
 
     //updates a uri if found, inserts if not found
-    ACTION newuri(name token_family, name license_owner, name uri_group, name uri_name, string new_uri, optional<uint64_t> serial);
+    ACTION newuri(name schema_name, name license_owner, name uri_group, name uri_name, string new_uri, optional<uint64_t> serial);
 
     //deletes a uri
-    ACTION deleteuri(name token_family, name license_owner, name uri_group, name uri_name, optional<uint64_t> serial);
+    ACTION deleteuri(name schema_name, name license_owner, name uri_group, name uri_name, optional<uint64_t> serial);
 
     //======================== fungible actions ========================
 
@@ -131,35 +149,39 @@ public:
     TABLE realmdata {
         string drealms_version;
         name realm_name;
-        vector<name> nonfungibles;
-        vector<symbol> fungibles;
+        vector<name> nonfungibles; //TODO?: rename to schemas
+        vector<symbol> fungibles; //TODO?: rename to currencies
     };
     typedef singleton<name("realmdata"), realmdata> realmdata_singleton;
 
     //scope: get_self().value
     //ram: ~507 bytes
-    TABLE family {
-        name family_name;
+    TABLE schema {
+        name schema_name;
         name issuer;
-        name license_model;
-        uint32_t min_license_length;
-        uint32_t max_license_length;
-        bool retirable;
-        bool transferable;
-        bool consumable;
+        
         uint64_t supply;
         uint64_t issued_supply;
         uint64_t max_supply;
 
-        uint64_t primary_key() const { return family_name.value; }
-        EOSLIB_SERIALIZE(family, 
-            (family_name)(issuer)(license_model)
-            (retirable)(transferable)(consumable)
-            (supply)(issued_supply)(max_supply))
-    };
-    typedef multi_index<name("families"), family> families_table;
+        name license_model;
+        uint32_t min_license_length;
+        uint32_t max_license_length;
 
-    //scope: family_name.value
+        map<name, bool> settings; //retirable, transferable, consumable, activatable
+        map<name, uint32_t> default_stats; //defaults used when minting a new nft
+        symbol exp_symbol;
+
+        uint64_t primary_key() const { return schema_name.value; }
+        EOSLIB_SERIALIZE(schema, 
+            (schema_name)(issuer)
+            (supply)(issued_supply)(max_supply)
+            (license_model)(min_license_length)(max_license_length)
+            (settings)(default_stats)(exp_symbol))
+    };
+    typedef multi_index<name("schemas"), schema> schemas_table;
+
+    //scope: schema_name.value
     //ram: ~303 bytes
     TABLE license {
         name owner;
@@ -173,17 +195,26 @@ public:
     };
     typedef multi_index<name("licenses"), license> licenses_table;
 
-    //scope: family_name.value
+    //scope: schema_name.value
     //ram: ~198 bytes
     TABLE nonfungible {
         uint64_t serial;
         name owner;
+
+        uint16_t level; //starts at level 1
+        asset experience; //EXP, SCALES, etc
+        asset next_level; //same formula for all members of schema (linear, quadratic, etc)
+        uint8_t unspent; //points to spend on upgrading stats
+        map<name, uint32_t> stats; //strength => 1, dexterity => 1, etc.
+
         map<name, string> relative_uris;
         map<name, string> checksums;
 
         uint64_t primary_key() const { return serial; }
         EOSLIB_SERIALIZE(nonfungible, 
-            (serial)(owner)(relative_uris)(checksums))
+            (serial)(owner)
+            (level)(experience)(next_level)(unspent)(stats)
+            (relative_uris)(checksums))
     };
     typedef multi_index<name("nfts"), nonfungible> nfts_table;
 
